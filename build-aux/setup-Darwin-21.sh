52d2e1f767aacda985eb01f5ff9be93e7592a522
thistleman <codes5096@gmail.com>
2022-06-07 14:05:24 -0700

    added conditional checks for symlinks due to differing bin locations

--- build-aux/setup-Darwin-21.sh
+++ build-aux/setup-Darwin-21.sh
@@ -8,9 +8,18 @@ brew doctor
 
 # build tools
 brew install autoconf automake libtool gnu-sed gawk
-[ ! -L /usr/bin/sed -o ! "$(readlink /usr/bin/sed)" == "/usr/bin/gsed" ] && mv /usr/bin/sed /usr/bin/sed-old
-[ ! -e /usr/bin/sed ] && ln -s /usr/bin/gsed /usr/bin/sed
-[ ! -e /usr/bin/libtoolize ] && ln -s /usr/bin/glibtoolize /usr/bin/libtoolize
+
+if test -e "/usr/local/bin"; then
+    [ ! -L /usr/local/bin/sed -o ! "$(readlink /usr/local/bin/sed)" == "/opt/homebrew/bin/gsed" ] && mv /usr/local/bin/sed /usr/local/bin/sed-old
+    [ ! -e /usr/local/bin/sed ] && ln -s /opt/homebrew/bin/gsed /usr/local/bin/sed
+    [ ! -e /usr/local/bin/libtoolize ] && ln -s /opt/homebrew/bin/glibtoolize /usr/local/bin/libtoolize
+fi
+
+if test -e "/usr/bin"; then
+    [ ! -L /usr/bin/sed -o ! "$(readlink /usr/bin/sed)" == "/opt/homebrew/bin/gsed" ] && mv /usr/bin/sed /usr/bin/sed-old
+    [ ! -e /usr/bin/sed ] && ln -s /opt/homebrew/bin/gsed /usr/bin/sed
+    [ ! -e /usr/bin/libtoolize ] && ln -s /opt/homebrew/bin/glibtoolize /usr/bin/libtoolize
+fi
 
 # install python3
 brew install python3
