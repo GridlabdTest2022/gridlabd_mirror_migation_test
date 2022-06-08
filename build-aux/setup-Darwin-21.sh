416753375865927f7c1dd085883183f496dfa0e0
thistleman <codes5096@gmail.com>
2022-06-08 09:59:45 -0700

    Adjusted to creating gridlabd directory and adding to bash profile path.

--- build-aux/setup-Darwin-21.sh
+++ build-aux/setup-Darwin-21.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 
-brew update || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-export PATH=/opt/homebrew/bin:$PATH
-export PATH=/opt/homebrew/sbin:$PATH
+brew update || NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    # Set-up install directory and add gridlabd and homebrew to bash profile path
+    if test ! -e "/opt/gridlabd/bin"; then
+        mkdir -p "/opt/gridlabd/bin"
+    fi
+
+    if test ! -e "~/.bash_profile"; then
+        touch "~/.bash_profile"
+    fi
+
+    if grep -q '/opt/gridlabd/bin' "~/.bash_profile"; then
+        touch "~/.bash_profile"
+        echo 'export PATH=/opt/homebrew/bin:$PATH' >> ~/.bash_profile
+        echo 'export PATH=/opt/homebrew/sbin:$PATH' >> ~/.bash_profile
+        echo 'export PATH=/opt/gridlabd/bin:$PATH' >> ~/.bash_profile
+        source ~/.bash_profile
+    fi
+
 brew doctor
 
 # build tools
+
     brew install autoconf automake libtool gnu-sed gawk
 
-    # Some MAC OS use /usr/bin and have not created a modifiable /usr/local/bin, therefore it needs appropriate checks before creating symlinks
-    if test ! -e "/usr/local/bin"; then
-        mkdir "/usr/local/bin"
-        [ ! -e /usr/local/bin/sed ] && ln -s /opt/homebrew/bin/gsed /usr/local/bin/sed
-        [ ! -e /usr/local/bin/libtoolize ] && ln -s /opt/homebrew/bin/glibtoolize /usr/local/bin/libtoolize
-    elif test -e "/usr/local/bin"; then
-        [ ! -L /usr/local/bin/sed -o ! "$(readlink /usr/local/bin/sed)" == "/opt/homebrew/bin/gsed" ] && mv /usr/local/bin/sed /usr/local/bin/sed-old
-        [ ! -e /usr/local/bin/sed ] && ln -s /opt/homebrew/bin/gsed /usr/local/bin/sed
-        [ ! -e /usr/local/bin/libtoolize ] && ln -s /opt/homebrew/bin/glibtoolize /usr/local/bin/libtoolize
-    fi
+    # Update symlinks in the gridlabd bin
+    [ ! -L /opt/gridlabd/bin/sed -o ! "$(readlink /opt/gridlabd/bin/sed)" == "/opt/homebrew/bin/gsed" ] && mv /opt/gridlabd/bin/sed /opt/gridlabd/bin/sed-old
+    [ ! -e /opt/gridlabd/bin/sed ] && ln -s /opt/homebrew/bin/gsed /opt/gridlabd/bin/sed
+    [ ! -e /opt/gridlabd/bin/libtoolize ] && ln -s /opt/homebrew/bin/glibtoolize /opt/gridlabd/bin/libtoolize
 
 # install python3
 brew install python3
@@ -29,7 +40,7 @@ brew install mdbtools
 # docs generators
 brew install mono
 brew install naturaldocs
-ln -s /opt/homebrew/bin/naturaldocs /usr/local/bin/natural_docs
+ln -s /opt/homebrew/bin/naturaldocs /opt/gridlabd/bin/natural_docs
 
 brew install doxygen
 
